b3dc4b49c3aaa98fb6ac934c3b47a9b98fdee066
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-05-28 18:42:00 +0300

    приступаю

--- src/content-agent/repository/channel/channel.repository.interface.ts
+++
@@ -1,7 +0,0 @@
-import {ChannelModel} from "../../model/channel/channel.model";
-import {InputModel} from "../../../file/model/input/input.model";
-
-export interface ChannelRepositoryInterface {
-    checkOneByChannelLink(data : InputModel) : Promise<ChannelModel>
-}
-
--- src/model/path/telegram.check.telegram.channel.path.interface.ts
+++
@@ -1,6 +0,0 @@
-import {PathInterface} from "./path.interface";
-//Надо ли так упарываться??
-//Надо подумать про ENUM, стоит ли сюда прикручивать
-export interface TelegramCheckTelegramChannelPathInterface extends PathInterface{
-    path : string
-}
