a5173864312cd099fac24d7d2759c0b151204aae
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-06-13 20:57:57 +0300

    fixed names

--- src/content-agent/action/run/get-posts-from-telegram-channel-file/run.get-posts-from-telegram-channel-file.config.model.ts
+++
@@ -1,7 +0,0 @@
-import {PathInterface} from "../../../../model/path/path.interface";
-
-
-//НАДО ДУМАЮ СДЕЛАТЬ ЭТО АБСТРАКТНЫМ КОНФИГОМ, Т.К. БУДУТ ФАЙЛЫ ЗАПУСКАТЬСЯ И МЫ ТУДА БУДЕМ ДАННЫЕ ПЕРЕДАВАТЬ
-export interface RunGetPostsFromTelegramChannelFileConfigModel {
-    pathToFile : PathInterface
-}
---
+++ src/request-converter/check-channels/check-channels.request-converter.module.ts
@@ -0,0 +1,17 @@
+import {CheckChannelsRequestModel} from "../../customer-manager/model/request/check-channels/check-channels.request.model";
+import {ChannelsToCheckInterface} from "../../customer-manager/model/channels-to-check.interface";
+import {Injectable} from "@nestjs/common";
+import {CheckChannelsRequestConverterInterface} from "./check-channels.request-converter.interface";
+
+@Injectable()
+export class CheckChannelsRequestConverter implements CheckChannelsRequestConverterInterface{
+    convert(request: CheckChannelsRequestModel): ChannelsToCheckInterface {
+        return {
+            channelsToCheck : request.links.map(link => {
+                return {
+                    channelToCheck : link
+                }
+            })
+        }
+    }
+}
